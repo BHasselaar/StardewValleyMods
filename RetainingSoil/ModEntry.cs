40fb1d14284df11f88ca69bf14ee00ec4fd5a83f
Yariazen <35210313+Yariazen@users.noreply.github.com>
2021-08-26 19:17:25 -0700

    Added SMAPI Events

--- RetainingSoil/ModEntry.cs
+++ RetainingSoil/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
 
 namespace RetainingSoil
 {
@@ -10,9 +11,21 @@ namespace RetainingSoil
             config = helper.ReadConfig<ModConfig>();
 
             helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
+            helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+            helper.Events.GameLoop.DayEnding += GameLoop_DayEnding;
         }
 
-        private void GameLoop_SaveLoaded(object sender, StardewModdingAPI.Events.SaveLoadedEventArgs e)
+        private void GameLoop_DayEnding(object sender, DayEndingEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
         {
             throw new System.NotImplementedException();
         }
