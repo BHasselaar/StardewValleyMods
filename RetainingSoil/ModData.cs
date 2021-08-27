494b3d60f216018cd84c48bf5ad8a6ac88427037
Yariazen <35210313+Yariazen@users.noreply.github.com>
2021-08-27 10:43:51 -0700

    Quick Update
    Still broken, will fix eventually

--- RetainingSoil/ModData.cs
+++ RetainingSoil/ModData.cs
@@ -1,6 +1,25 @@
-﻿namespace RetainingSoil
+﻿using Microsoft.Xna.Framework;
+using RetainingSoil.framework;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace RetainingSoil
 {
-    class ModData
+    public class ModData
     {
+        public List<Dictionary<Vector2, TileData>> data { get; set; }
+
+        public ModData()
+        {
+            data = new List<Dictionary<Vector2, TileData>>();
+        }
+        public void SaveData(Dictionary<GameLocation, List<Vector2>> dictionary)
+        {
+            this.data = new List<TileData>();
+            foreach (KeyValuePair<GameLocation, List<Vector2>> entry in dictionary)
+            {
+                this.data.Add(new TileData(entry.Key.name, entry.Value));
+            }
+        }
     }
 }
--- RetainingSoil/ModEntry.cs
+++ RetainingSoil/ModEntry.cs
@@ -1,14 +1,22 @@
-﻿using StardewModdingAPI;
+﻿using Microsoft.Xna.Framework;
+using RetainingSoil.framework;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.TerrainFeatures;
+using System.Collections.Generic;
 
 namespace RetainingSoil
 {
     public class ModEntry : Mod
     {
-        public ModConfig config;
+        public ModConfig modConfig;
+        public ModData modData;
+
         public override void Entry(IModHelper helper)
         {
-            config = helper.ReadConfig<ModConfig>();
+            modConfig = this.Helper.ReadConfig<ModConfig>();
+            modData = this.Helper.Data.ReadSaveData<ModData>("RetainingSoil") ?? new ModData();
 
             helper.Events.GameLoop.SaveLoaded += GameLoop_SaveLoaded;
             helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
@@ -17,17 +25,81 @@ namespace RetainingSoil
 
         private void GameLoop_DayEnding(object sender, DayEndingEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (Game1.IsMasterGame)
+            {
+                Dictionary<GameLocation, List<TileData>> data = new Dictionary<GameLocation, List<TileData>>();
+                foreach (GameLocation location in getAllLocationsAndBuildings())
+                {
+                    if (location != null)
+                    {
+                        if (!data.ContainsKey(location))
+                        {
+                            data.Add(location, new List<TileData>());
+                        }
+                        List<TileData> list = new List<TileData>();
+                        foreach (Dictionary<Vector2, TerrainFeature> dictionary in location.terrainFeatures)
+                        {
+                            foreach (KeyValuePair<Vector2, TerrainFeature> entry in dictionary)
+                            {
+                                if (entry.Value is HoeDirt)
+                                {
+                                    if ((((HoeDirt)entry.Value).fertilizer == 370 ||
+                                         ((HoeDirt)entry.Value).fertilizer == 371 ||
+                                         ((HoeDirt)entry.Value).fertilizer == 920) &&
+                                         ((HoeDirt)entry.Value).state == 1)
+                                    {
+                                        foreach (Dictionary<Vector2, TileData> tileData in data[location])
+                                        {
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        data[location] = list;
+                    }
+                }
+                modData.LoadData(data);
+                this.Helper.Data.WriteSaveData("RetainingSoil", modData);
+            }
         }
 
         private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (Game1.IsMasterGame)
+            {
+                if (Game1.dayOfMonth == 1)
+                {
+                    //clear data
+                }
+            }
         }
 
         private void GameLoop_SaveLoaded(object sender, SaveLoadedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (Game1.IsMasterGame)
+            {
+                modData = this.Helper.Data.ReadSaveData<ModData>("key") ?? new ModData();
+                restoreHoeDirt();
+            }
+        }
+
+        private void saveFertilizedDirt()
+        {
+
+        }
+
+        private void restoreHoeDirt()
+        {
+
+        }
+
+        private List<GameLocation> getAllLocationsAndBuildings()
+        {
+            List<GameLocation> locations = new List<GameLocation>();
+            locations.Add(Game1.getFarm());
+            //locations.Add(Game1.getLocationFromName("Greenhouse")); Later thing
+            return locations;
         }
     }
 }
